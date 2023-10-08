6ed6eefe930a14d6d8704cbe5c583b6ebfa708f6
Kim-Jae-He <jgim78628@gmail.com>
2023-10-09 02:26:35 +0900

    10-09

--- 문제/N1000번.js
+++ 문제/N1000번.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 // const input = fs.readFileSync("/dev/stdin").toString().split(' ');
 
 //문제풀이용
-const input = fs.readFileSync('example.txt').toString().split(' ');
+// 입력에 공백에 있어서 split를 사용하였습니다
+const input = fs.readFileSync('example.txt').toString().split(' '); 
+
 
 var a = parseInt(input[0]);
 var b = parseInt(input[1]);
