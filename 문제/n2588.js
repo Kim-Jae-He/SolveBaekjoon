a4892d5c349374e42d316c84fafff1ed7552d227
Kim-Jae-He <jgim78628@gmail.com>
2023-10-17 11:45:12 +0900

    10-17

--- 문제/n2588.js
+++ 문제/n2588.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //백준 제출용
-// const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+// const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 //문제풀이용
 const input = fs.readFileSync('example.txt').toString().split('\n');
@@ -9,10 +9,7 @@ const input = fs.readFileSync('example.txt').toString().split('\n');
 const [a, b] = input;
 const [q, w, e] = b;
 
-console.log(a);
-console.log(b[q]);
-
-console.log(parseInt(a) * parseInt(b[2]));
+console.log(a * b[2]);
 console.log(a * b[1]);
 console.log(a * b[0]);
 console.log(a * b);
