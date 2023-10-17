5b28ee693c0ab8a2940fcb0200e531d5b26f7bd9
Kim-Jae-He <jgim78628@gmail.com>
2023-10-17 13:57:46 +0900

    10-17 입출력과 사칙연산 완료

---
+++ 문제/1단계/n10171.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+//백준 제출용
+// const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+
+//문제풀이용
+const input = fs.readFileSync('example.txt').toString().split(' ');
+
+
+
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|`)
