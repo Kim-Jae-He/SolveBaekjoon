c28984ac0bcf0e4b2e9a0810803b0f8fc8b39dfa
Kim-Jae-He <jgim78628@gmail.com>
2023-12-17 20:13:20 +0900

    12-17

--- test/5단계/n27866.js
+++ test/5단계/n27866.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 //백준 문제제출
-//const input = fs.readFileSync('dev/stdin').toString().split('\n');
+//const input = fs.readFileSync('dev/stdin').toString().trim();
 
 //문제풀이용
-const input = fs.readFileSync('answer/example.txt').toString().split('\n');
+const input = fs.readFileSync('answer/example.txt').toString().trim();
 
 let a = input[0].toString();
 
