24f1cf9408bca26adc7b6c471cba7d2d4eeffc4c
Kim-Jae-He <jgim78628@gmail.com>
2023-12-21 17:35:24 +0900

    12-21

--- test/5단계/n10809.js
+++ test/5단계/n10809.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 
-//백준제출용
-//const input = fs.readFileSync('/dev/stdin).toString();
+try {
+  //백준제출용
+  //const input = fs.readFileSync('/dev/stdin).toString();
 
-//문제풀이용
-const input = require('fs').readFileSync('answer/n10809.txt').toString();
-let answer = [];
+  //문제풀이용
+  const input = require('fs').readFileSync('answer/n10809.txt').toString();
+  let answer = [];
 
-for (i = 97; i <= 122; i++) {
-  answer.push(input.indexOf(String.fromCharCode(i)));
+  for (i = 97; i <= 122; i++) {
+    answer.push(input.indexOf(String.fromCharCode(i)));
+  }
+
+  console.log(answer.join(' '));
+} catch (err) {
+  console.log(err.message);
 }
-
-console.log(answer.join(' '));
